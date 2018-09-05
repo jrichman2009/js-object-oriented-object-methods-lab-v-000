6c8fce0e60268df0b03b8b069dc1db6aae121b53
Jeffrey Richman <jrichman2009@gmail.com>
2018-09-05 01:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember(name, homestate, training) {
   this.name = name
   this.homeState = homestate
   this.training = training
-  this.veto = function {
+  this.veto = function() {
     return "No I must disagree"
   }
 
